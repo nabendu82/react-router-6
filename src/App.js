bb07e68aa96bc53b9778a500e55778ac4866e8fb
Nabendu Biswas <nabendu.biswas@gmail.com>
2021-11-18 17:08:22 +0530

    React-router 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+import AllCourses from "./components/AllCourses";
+import Bundles from "./components/Bundles";
+import Courses from "./components/Courses";
+import Home from "./components/Home";
+import SingleCourse from "./components/SingleCourse";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/apps" element={<Navigate replace to="/courses" />}/>
+        <Route path="/courses" element={<Courses />}>
+          <Route path="allcourses" element={<AllCourses />}>
+            <Route path=":id" element={<SingleCourse />}/>
+          </Route>
+          <Route path="bundles" element={<Bundles />} />
+        </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/SingleCourse.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom';
+
+const SingleCourse = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    return (
+    <div className="card">
+        <div className="card-body">
+            <h5 className="card-title">Course Name</h5>
+            <p className="card-text">{id}</p>
+            <a onClick={() => navigate("/dashboard", { state: id })} class="card-link">
+                More Info
+            </a>
+            <Link to="/dashboard" state={"ReactJS Testing"} class="card-link">
+                Test Course
+            </Link>
+        </div>
+    </div>
+    )
+}
+
+export default SingleCourse
